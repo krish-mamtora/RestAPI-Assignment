57d651003d166e3d82b4cde617b913c75dbee4f7
krish-mamtora <krish.mamtora@roimaint.com>
2026-02-04 18:11:50 +0530

    EmployeeHeader Versioning changed to constructor injection and give proper retun type in api versioning

--- src/main/java/com/example/RestAPI2/controller/EmployeeHeaderVersioning.java
+++ src/main/java/com/example/RestAPI2/controller/EmployeeHeaderVersioning.java
@@ -1,7 +1,6 @@
 package com.example.RestAPI2.controller;
 
 import com.example.RestAPI2.service.VersionTestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,19 +11,27 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class EmployeeHeaderVersioning {
-    @Autowired
-    VersionTestService versionTestService ;
 
+    private VersionTestService versionTestService ;
+    public EmployeeHeaderVersioning(VersionTestService versionTestService){
+        this.versionTestService = versionTestService;
+    }
     @GetMapping(value = "/employees" , headers = "api-version=1")
-    public ResponseEntity<?> v1(){
-        return ResponseEntity.ok(
-                Map.of("version", "v1","data", versionTestService.getEmployeeV1())
+    public ResponseEntity<Map<String, Object>> v1(){
+        Map<String, Object> body = Map.of(
+                "version", "v1",
+                "data", versionTestService.getEmployeeV1()
         );
+        return ResponseEntity.ok(body);
     }
     @GetMapping(value = "/employees" , headers = "api-version=2")
-    public ResponseEntity<?>v2(){
-        return ResponseEntity.ok(
-                Map.of("version", "v2","data", versionTestService.getEmployeeV2())
+    public ResponseEntity<Map<String, Object>> v2(){
+        Map<String, Object> body = Map.of(
+                "version", "v2",
+                "data", versionTestService.getEmployeeV2()
         );
+        return ResponseEntity.ok(body);
     }
 }
+
+
